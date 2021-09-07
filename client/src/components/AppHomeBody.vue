5b2885776671de00d207b1fa02eb101812676822
Krishna Nayak <nayak100krishna@gmail.com>
2021-09-07 20:29:07 +0530

    feature added

--- client/src/components/AppHomeBody.vue
+++ client/src/components/AppHomeBody.vue
@@ -55,7 +55,51 @@
     </div>
 
     <!-- Features box -->
-    <div></div>
+    <div class="bg-white relative">
+      <div class="py-24 px-6 grid place-items-center grid-cols-2 md:grid-cols-3 auto-rows-auto gap-4">
+        <div class="p-3 h-80 max-w-sm" id="Data" v-for="feature in features" :key="feature.title">
+          <img :src="feature.src" alt="" class="w-12" />
+          <h1 class="py-1 font-bold">{{ feature.title }}</h1>
+          <p class="text-gray-400 font-extralight">{{ feature.desc }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Security -->
+    <div class="relative bg-blue-200 text-center py-32">
+      <h1 class="text-3xl font-bold md:text-5xl">Unmatched security</h1>
+      <div class="py-24 px-6 grid place-items-center grid-cols-1 md:grid-cols-3 auto-rows-auto gap-4">
+        <div class="max-w-sm h-60 bg-white p-5 rounded" v-for="security in securityData" :key="security.title">
+          <img :src="security.src" alt="" class="w-12 mx-auto my-4" />
+          <h1 class="text-xl">{{ security.title }}</h1>
+          <p class="font-extralight">{{ security.desc }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Say hello -->
+    <div class="relative bg-gray-500 text-center">
+      <div>
+        <h1>Say hello to the most convenient way to give money to your kids</h1>
+        <div v-for="feature in features" :key="feature"></div>
+      </div>
+    </div>
+
+    <!-- Footer above -->
+    <div class="relative bg-gray-100 py-28 px-5">
+      <div class="flex justify-evenly md:flex-row flex-col items-center gap-5">
+        <div class="text-center">
+          <h1 class="text-2xl md:text-3xl font-bold">Bla Bla is Powered by</h1>
+          <img :src="logoPoweredBy" alt="" class="w-20 mx-auto" />
+        </div>
+        <div class="max-w-lg">
+          <p class="text-justify font-light">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ratione autem nulla dignissimos cumque culpa dolore officia id placeat animi. Eligendi, voluptatibus
+            provident.
+          </p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -63,6 +107,7 @@
 // @ is an alias to /src
 /* eslint-disable vue/no-unused-components */
 /* eslint-disable no-unused-vars */
+/* eslint-disable */
 
 import HelloWorld from "@/components/HelloWorld.vue";
 import Intro from "@/layouts/Intro.vue";
@@ -74,6 +119,58 @@ export default {
     HelloWorld,
     Intro,
     Card,
+  },
+
+  data() {
+    return {
+      logoUrl: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+      logoPoweredBy: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+      features: [
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Personal Prepaid Card",
+          desc: "Letrasre recently with desktop publishing ions of Lorem Ipsum.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Rewards",
+          desc: "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing ions of Lorem Ipsum.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Tasks",
+          desc: "Letraset sheets containing Lorem Ipsum passages, an publishing ions of Lorem Ipsum.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Splits Bill",
+          desc: "Letraset sheets containing Losktop publishing ions of Lorem Ipsum.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Shop",
+          desc: "Letraset sheets containing Lorem Ipsth desktop publishing ions of Lorem Ipsum.",
+        },
+      ],
+
+      securityData: [
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Numberless Card",
+          desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "The safest transaction",
+          desc: "ummy text of the printing and typesetting industry.",
+        },
+        {
+          src: "https://wireframestogo.com/585b-Image-Placeholder-Light/thumbnail.png",
+          title: "Block/pause the card",
+          desc: "Lorem Ipsum is simply duminting and typesetting industry.",
+        },
+      ],
+    };
   },
 };
 </script>
